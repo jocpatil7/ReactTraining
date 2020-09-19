0f04d0d769a2415e450e04a2bfcf714a0221c96e
jocpatil7 <jocpatil7@gmail.com>
2020-09-19 21:37:36 +0530

    3 files modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>HTML Starter</h1>`;
+appDiv.innerHTML = `<h1>Welcome To All </h1>`;
